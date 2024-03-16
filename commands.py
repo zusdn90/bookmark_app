a6c02f18dac04da146ec9da971efb86f6616092f
zusdn90 <zusdn90@naver.com>
2024-03-16 21:39:32 +0900

    Github 북마크 데이터 가져오기 기능 추가

--- commands.py
+++ commands.py
@@ -18,8 +18,8 @@ class CreateBookmarksTableCommand:
         })
 
 class AddBookmarkCommand:
-    def execute(self, data):
-        data['data_added'] = datetime.utcnow().isoformat()
+    def execute(self, data, timestamp=None):
+        data['data_added'] = timestamp or datetime.utcnow().isoformat()
         db.add('bookmarks', data)
         return 'Bookmark added!'
 
@@ -34,6 +34,57 @@ class DeleteBookmarksCommand:
     def execute(self, data):
         db._execute('bookmarks', {'id': data})
         return 'Bookmark deleted!'
+
+
+class ImportGitHubStarsCommand:
+    def _extract_bookmark_info(self, repo):  # <1>
+        return {
+            'title': repo['name'],
+            'url': repo['html_url'],
+            'notes': repo['description'],
+        }
+
+    def execute(self, data):
+        bookmarks_imported = 0
+
+        github_username = data['github_username']
+        next_page_of_results = f'https://api.github.com/users/{github_username}/starred'  # <2>
+
+        while next_page_of_results:  # <3>
+            stars_response = requests.get(  # <4>
+                next_page_of_results,
+                headers={'Accept': 'application/vnd.github.v3.star+json'},
+            )
+            next_page_of_results = stars_response.links.get('next', {}).get('url')  # <5>
+
+            for repo_info in stars_response.json():
+                repo = repo_info['repo']  # <6>
+
+                if data['preserve_timestamps']:
+                    timestamp = datetime.strptime(
+                        repo_info['starred_at'],  # <7>
+                        '%Y-%m-%dT%H:%M:%SZ'  # <8>
+                    )
+                else:
+                    timestamp = None
+
+                bookmarks_imported += 1
+                AddBookmarkCommand().execute(  # <9>
+                    self._extract_bookmark_info(repo),
+                    timestamp=timestamp,
+                )
+
+        return f'Imported {bookmarks_imported} bookmarks from starred repos!'  # <10>
+
+
+class EditBookmarkCommand:
+    def execute(self, data):
+        db.update(
+            'bookmarks',
+            {'id': data['id']},
+            data['update'],
+        )
+        return 'Bookmark updated!'
 
 class QuitCommand:
     def execute(self):
